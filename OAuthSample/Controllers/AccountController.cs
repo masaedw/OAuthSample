7fb5b52433eb01b1d4db5e2129a974ed8ace41d2
Masayuki Muto <muto@timedia.co.jp>
2012-02-11 14:01:25 +0900

    Add Account and Message Controllers

---
+++ OAuthSample/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OAuthSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OAuthSample/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OAuthSample.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
