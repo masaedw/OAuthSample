158692a9b0baf7322214a97d1c4cf462f2d2d67f
Masayuki Muto <muto@timedia.co.jp>
2012-02-19 18:50:17 +0900

    Add User model

---
+++ OAuthSample/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OAuthSample.Models
+{
+    public class User
+    {
+        public string TwitterAccessToken { get; set; }
+        public string TwitterAccessTokenSecret { get; set; }
+        public dynamic TwitterUser { get; set; }
+
+        public string FacebookAccessToken { get; set; }
+        public dynamic FacebookUser { get; set; }
+    }
+}
